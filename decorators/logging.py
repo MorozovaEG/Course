433a60ecf49aac79b8043cc5adeeb47caf2e0378
MorozovaEG <morozovaeg@yandex.ru>
2024-04-07 13:34:07 +0300

    del

--- decorators/logging.py
+++
@@ -1,8 +0,0 @@
-def logging(func):
-    """
-    Декоратор, который выводит параметры с которыми была вызвана функция
-    """
-
-    def wrapper(*args, **kwargs):
-      print(f"Функция вызвана с параметрами: {*args}, {**kwargs}")
-    return wrapper
